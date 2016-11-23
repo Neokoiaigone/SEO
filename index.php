4fa7af6ff7fd8a59c6b7c76cc039d659da5a0bf6
Unknown <louisalexandrebricq@yahoo.fr>
2016-11-23 15:59:55 +0100

    intégration la base

---
+++ index.php
@@ -0,0 +1,231 @@
+<html>
+<head>
+    <title>Intégration</title>
+    <meta charset="">
+    <meta name="viewport" content="width=device-width">
+
+    <!--style -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    
+
+
+</head>
+
+<body>
+
+
+<header id="header">
+    <nav class=" navbar navbar-default">
+        <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand animated FadeInDown" href="#">Cuda</a>
+            </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right animated FadeInDown">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Compétences</a></li>
+                    <li><a href="#">Equipe</a></li>
+                    <li><a href="#">Contact</a></li>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="text-center">
+                <h1 class="animated FadeInDown">
+                    LOUIS-ALEXANDRE BRICQ !
+                </h1> 
+                <a href="#" class="btn btn-warning btn-lg btn btn-6 btn-6d">PROJETS REALISES</a>
+            </div>  
+        </div>
+    </div>
+</div>
+
+</header>
+
+<section class="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center wow FadeInDown">
+                    <h2>MON SAVOIR FAIRE</h2>
+                    <hr>
+                    <p>Voici les savoir faires que j'ai utilisés jusqu'à maintenant à l'iim pour créer mes sites internet. </p>
+                </div>  
+            </div>
+            <div class="col-md-3">
+                <div class="service text-center wow FadeInLeft">
+                    <img src="images/php.png" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>À quoi ça sert ?</h3>
+                    <p>Forums, blogs, réseaux sociaux... les sites Web d'aujourd'hui sont fonctionnels, participatifs, ils se renouvellent sans cesse. Ils sont « dynamiques ». Le langage PHP a été créé justement pour générer cette interactivité. </p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="service text-center wow FadeInLeft">
+                    <img src="images/css.png" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>À quoi ça sert ?</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="service text-center wow FadeInRight">
+                    <img src="images/wordpress.png" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>À quoi ça sert ?</h3>
+                    <p>WordPress est un système de gestion de contenus permettant de créer son propre site assez facilement. En plus de sa simplicité d'utilisation, il présente de nombreux autres atouts.  </p>
+                </div>
+            </div>
+            <div class="col-md-3 ">
+                <div class="service text-center wow FadeInRight">
+                    <img src="images/js.png" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>À quoi ça sert ?</h3>
+                    <p>Faire bouger, apparaitre ou disparaitre des éléments de la page (un titre, un menu, un paragraphe, une image…).
+                    Mettre à jour des éléments de la page sans recharger la page (changer le texte, recalculer un nombre, etc).
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="rencontres">
+    <div class="container">
+        <div class="row">
+                <div class=" text-center wow FadeInDown">
+                    <h2>MES COLLABORATEURS</h2>
+                    <hr>
+                    <p>Voici une présentation de moi même et des personnes avec qui je travail souvent.</p>
+                </div>  
+            <div class="col-md-3">
+                <div class="rencontre text-center wow FadeInDown">
+                    <img src="images/moi.jpg" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>Futur webmaster</h3>
+                    <p class="blue">Louis-Alexandre Bricq</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/facebook.svg" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/twitter.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/mail2.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/insta.png" class="icone"></a>
+                </div>
+            </div>
+             <div class="col-md-3">
+                <div class="rencontre text-center wow FadeInUp">
+                    <img src="images/bg.jpg" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>Graphiste de génie</h3>
+                    <p class="blue">Adam benhaddou</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/facebook.svg" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/twitter.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/mail2.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/insta.png" class="icone"></a>
+                </div>
+            </div>
+             <div class="col-md-3">
+                <div class="rencontre text-center wow FadeInUp">
+                    <img src="images/lol.jpeg" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>service 1</h3>
+                    <p class="blue">Creative Director</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/facebook.svg" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/twitter.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/mail2.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/insta.png" class="icone"></a>
+                </div>
+            </div>
+             <div class="col-md-3">
+                <div class="rencontre text-center wow FadeInDown">
+                    <img src="images/lol.jpeg" alt="" class="img-responsive roundedImage center-block"/>
+                    <h3>service 1</h3>
+                    <p class="blue">Creative Director</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/facebook.svg" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/twitter.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/mail2.png" class="icone"></a>
+                    <a href="https://www.facebook.com/Pnlmusic/?fref=ts"><img src="images/insta.png" class="icone"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+
+<div class="contactus">
+    <selection id="contact">
+        <div class="container" id="contactus">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="text-center">
+                        <h2 class="contact-us wow FadeInDown">
+                            CONTACTEZ NOUS
+                        </h2> 
+                        <hr>
+                        <p class="adressecontact wow FadeInDown"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <div class="contacter col-md-12 col-sm-12">
+                            <div class="name col-xs-6 wow FadeInLeft">
+                                <input type="text" class="form-control" placeholder="* Your Name">
+                            </div>
+                            <div class="email1 col-xs-6 wow FadeInRight">
+                                <input type="text" class="form-control" placeholder="* Your Email">
+                            </div>
+                            <div class="email col-xs-12 wow FadeInLeft">
+                                <textarea name="msg" row="6" placeholder="* Your message" ></textarea>
+                            </div>
+
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </selecton>
+</div>
+
+<footer class="wow fadeIn navbar-default footer wow FadeInDown">
+    <div class="footer-above background-footer-color">
+        <div class="container">
+            <div class="row">
+                    <div class="footer-col col-md-12">
+                        <div class="text-center">
+                            <ul class="list-inline">
+                                <li><a href="#">Facebook</a></li>
+                                <li><a href="#">Twitter</a></li>
+                                <li><a href="#">Google+</a></li>
+                                <li><a href="#">Linkedin</a></li>
+                                <li><a href="#">Behance</a></li>
+                                <li><a href="#">Dribbble</a></li>
+                                <li><a href="#">Github</a></li>  
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+              
+</body>
+</html>
